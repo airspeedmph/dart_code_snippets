094aa2266bffe793ed8644dba32b85f45228acbe
sid bhosale <bhosalesid777@gmail.com>
2025-03-25 02:11:31 +0530

    added new funtion

--- functions_25_mar_24.dart
+++ functions_25_mar_24.dart
@@ -2,6 +2,8 @@ void main(List<String> args) {
   var a = findArea(1, 35);
   print(a);
   print(secondFindArea(34, 65));
+  int rectArea = getArea(15, 33);
+  print("area of the rectangle is $rectArea");
 }
 
 int findArea(int length, int width) {
@@ -10,4 +12,9 @@ int findArea(int length, int width) {
 
 secondFindArea(int length, int width) {
   print(length * width);
+}
+
+int getArea(int length, int width) {
+  int area = length * width;
+  return area;
 }
