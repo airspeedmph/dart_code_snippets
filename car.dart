37aebdc9c5d71bbf97f2ca53d144b0ed47959d56
sid bhosale <bhosalesid777@gmail.com>
2025-03-26 12:17:55 +0530

    refactor

---
+++ car.dart
@@ -0,0 +1,13 @@
+
+
+// fat arrow short hand expression syntax 
+void main(List<String> args) {
+  perimeter(24, 63);
+}
+
+void perimeter(int length, int width) =>
+    print("the perimeter is ${length * width}");
+    //we can only use this syntax when there is only one single line of expression 
+
+
+slkjflknneeficjsjj ojweopr  fsfsfsfsfsfsfsfsfsfsfsfsfsfsfsffsfsfsfs
